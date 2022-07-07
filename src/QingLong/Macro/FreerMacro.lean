5ba3103bd4c73c30cde6249ca366cf8ec202202a
Gary Haussmann <gjhaussmann@gmail.com>
2022-07-07 16:59:59 -0600

    freeName -> freerName

--- src/QingLong/Macro/FreerMacro.lean
+++ src/QingLong/Macro/FreerMacro.lean
@@ -2,7 +2,7 @@
 import Lean
 import Lean.Parser
 
-import QingLong.Macro.SumMacro
+-- we need to instantiate a Sendable instance for getNamed/putNamed
 import QingLong.Data.NamedState
 
 namespace Freer
@@ -10,7 +10,6 @@ namespace Freer
 open Lean Elab Command Term Meta 
 open Parser.Term
 
-open SumMacro
 open NamedState
 
 
@@ -33,31 +32,31 @@ macro "mkFmap" freerName:ident fmapName:ident f:ident : command => do
 
 
 set_option hygiene false in
-macro "mkFFunctor" freeName:ident fmapName:ident f:ident : command => do
-  let c1c : Syntax := Lean.mkIdent (freeName.getId ++ "Pure")
-  let c2c : Syntax := Lean.mkIdent (freeName.getId ++ "Impure")
+macro "mkFFunctor" freerName:ident fmapName:ident f:ident : command => do
+  let c1c : Syntax := Lean.mkIdent (freerName.getId ++ "Pure")
+  let c2c : Syntax := Lean.mkIdent (freerName.getId ++ "Impure")
   let c1pat ← `(matchAltExpr| | $c1c a => $c1c a)
   let c2pat ← `(matchAltExpr| | $c2c fa => $c2c (Functor.map (xm f) fa))
   let branches := #[c1pat,c2pat]
-  `(instance [Functor $f] : Functor $freeName where
+  `(instance [Functor $f] : Functor $freerName where
         map := $fmapName)
 
-def mkFMonadFunc (freeName : Syntax) (bindName : Syntax) (f :Syntax) : CommandElabM Unit := do
-  let c1c : Syntax := Lean.mkIdent (freeName.getId ++ "Pure")
-  let c2c : Syntax := Lean.mkIdent (freeName.getId ++ "Impure")
+def mkFMonadFunc (freerName : Syntax) (bindName : Syntax) (f :Syntax) : CommandElabM Unit := do
+  let c1c : Syntax := Lean.mkIdent (freerName.getId ++ "Pure")
+  let c2c : Syntax := Lean.mkIdent (freerName.getId ++ "Impure")
   let c1pat ← `(matchAltExpr| | $c1c a => f a)
   let c2pat ← `(matchAltExpr| | $c2c fa next => $c2c fa (fun z => $bindName (next z) f))
   let branches := #[c1pat,c2pat]
   let matchTerm ← `(match m with $branches:matchAlt*)
-  let bindDecl ← `(def $bindName {α β : Type} (m : $freeName α) (f : α → $freeName β) : $freeName β := $matchTerm)
+  let bindDecl ← `(def $bindName {α β : Type} (m : $freerName α) (f : α → $freerName β) : $freerName β := $matchTerm)
   elabCommand bindDecl
   let monadI ←
-            `(instance : Monad $freeName where
+            `(instance : Monad $freerName where
                 pure := $c1c
                 bind := $bindName)
   elabCommand monadI
 
-elab "mkFMonad" freeName:ident bindName:ident f:ident : command => mkFMonadFunc freeName bindName f
+elab "mkFMonad" freerName:ident bindName:ident f:ident : command => mkFMonadFunc freerName bindName f
 
 
 class Sendable (b : Type → Type) (m : Type → Type 1) where
@@ -70,66 +69,46 @@ def putNamed (n : String) {v : Type} (x : v) {m : Type → Type 1} [Sendable (Na
     Sendable.send <| @NamedState.Put n v x
 
 
-def mkSendableFunc (freeName : Syntax) (f : Syntax) : CommandElabM Unit := do
-  let c1c : Syntax := Lean.mkIdent (freeName.getId ++ "Pure")
-  let c2c : Syntax := Lean.mkIdent (freeName.getId ++ "Impure")
+def mkSendableFunc (freerName : Syntax) (f : Syntax) : CommandElabM Unit := do
+  let c1c : Syntax := Lean.mkIdent (freerName.getId ++ "Pure")
+  let c2c : Syntax := Lean.mkIdent (freerName.getId ++ "Impure")
   let cd ←
-          `(instance {b : Type → Type} [Prismatic b $f] : Sendable b $freeName where
+          `(instance {b : Type → Type} [Prismatic b $f] : Sendable b $freerName where
               send := fun v => $c2c (Prismatic.inject v) $c1c)
   elabCommand cd
 
-def mkInterpreterFunc (freeName : Syntax) (sumName : Syntax) (interpretName : Syntax) : CommandElabM Unit := do
-  let c1c : Syntax := Lean.mkIdent (freeName.getId ++ "Pure")
-  let c2c : Syntax := Lean.mkIdent (freeName.getId ++ "Impure")
+def mkInterpreterFunc (freerName : Syntax) (sumName : Syntax) (interpretName : Syntax) : CommandElabM Unit := do
+  let c1c : Syntax := Lean.mkIdent (freerName.getId ++ "Pure")
+  let c2c : Syntax := Lean.mkIdent (freerName.getId ++ "Impure")
   let c1pat ← `(matchAltExpr| | $c1c a => pure a)
   let c2pat ← `(matchAltExpr| | $c2c v next => bind (c v) (fun xx => $interpretName c (next xx)))
   let branches := #[c1pat,c2pat]
   let cd ←
-          `(def $interpretName {α : Type} {n : Type → Type} [Monad n] (c : {z : Type} → $sumName z → n z) (m : $freeName α) : n α :=
+          `(def $interpretName {α : Type} {n : Type → Type} [Monad n] (c : {z : Type} → $sumName z → n z) (m : $freerName α) : n α :=
               match m with $branches:matchAlt*)
   elabCommand cd
   
 
-def mkFreerFunc (freeName : Syntax) (f : Syntax) : CommandElabM Unit := do
-  let mapName : Syntax := Lean.mkIdent <| Name.mkSimple <| freeName.getId.toString ++ "mapX"
-  let bindName : Syntax := Lean.mkIdent <| Name.mkSimple <| freeName.getId.toString ++ "bindX"
-  let bindIxName : Syntax := Lean.mkIdent <| Name.mkSimple <| freeName.getId.toString ++ "bindXX"
-  let reindexName : Syntax := Lean.mkIdent <| Name.mkSimple <| freeName.getId.toString ++ "reindexX"
-  let interpreterName : Syntax := Lean.mkIdent <| Name.mkSimple <| "run" ++ freeName.getId.toString
-  let m1 ← `(mkFreerInductive $freeName $f)
+def mkFreerFunc (freerName : Syntax) (f : Syntax) : CommandElabM Unit := do
+  let mapName : Syntax := Lean.mkIdent <| Name.mkSimple <| freerName.getId.toString ++ "mapX"
+  let bindName : Syntax := Lean.mkIdent <| Name.mkSimple <| freerName.getId.toString ++ "bindX"
+  let bindIxName : Syntax := Lean.mkIdent <| Name.mkSimple <| freerName.getId.toString ++ "bindXX"
+  let reindexName : Syntax := Lean.mkIdent <| Name.mkSimple <| freerName.getId.toString ++ "reindexX"
+  let interpreterName : Syntax := Lean.mkIdent <| Name.mkSimple <| "run" ++ freerName.getId.toString
+  let m1 ← `(mkFreerInductive $freerName $f)
   elabCommand m1
-  let m2 ← `(mkFmap $freeName $mapName $f)
+  let m2 ← `(mkFmap $freerName $mapName $f)
   elabCommand m2
-  let m3 ← `(mkFFunctor $freeName $mapName $f)
+  let m3 ← `(mkFFunctor $freerName $mapName $f)
   elabCommand m3
-  mkFMonadFunc freeName bindName f
-  mkSendableFunc freeName f
-  mkInterpreterFunc freeName f interpreterName
+  mkFMonadFunc freerName bindName f
+  mkSendableFunc freerName f
+  mkInterpreterFunc freerName f interpreterName
   /-
-  let m7 ← `(mkInterpreter $freeName $f $interpreterName)
+  let m7 ← `(mkInterpreter $freerName $f $interpreterName)
   elabCommand m7
   -/
 
-elab "mkFreer" freeName:ident f:ident : command => mkFreerFunc freeName f
-
-/-
-mkSumType Argh >| Id, IO |<
-set_option pp.rawOnError true
-mkFreer SomeFreer Argh
-#print SomeFreer
-#check runSomeFreer
-
-#check (do SomeFreer.Pure (); SomeFreer.Pure 4 : SomeFreer Nat)
-
-open Sendable in
-def x {m : Type → Type 1} [Monad m] [Sendable IO m] : m Nat:= do
-    send <| IO.println 3
-    pure 3
-
-def runSome := @x SomeFreer
-
-#check runSome
-#eval runSome
--/
+elab "mkFreer" freerName:ident f:ident : command => mkFreerFunc freerName f
 
 end Freer
